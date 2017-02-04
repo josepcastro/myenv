3549dd8a502e328dc6b064f88900a36dc51f1db3
josepcastro <castrosalvadojosep@gmail.com>
2017-02-04 22:05:01 +0000

    EASY INSTALL script

---
+++ install.sh
@@ -0,0 +1,7 @@
+mkdir ~/.vim
+mkdir ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+ln -s ~/myenv/.gitconfig ~/.gitconfig
+ln -s ~/myenv/.vimrc ~/.vimrc
+ln -s ~/myenv/.zshrc ~/.zshrc
